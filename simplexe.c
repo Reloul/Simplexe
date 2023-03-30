0a939c64da3ec35173a3e249ebe7b631118f2b91
Valentin Serres <108737662+Reloul@users.noreply.github.com>
2023-03-30 08:01:17 +0200

    Add files via upload

--- simplexe.c
+++ simplexe.c
@@ -1,6 +1,6 @@
 /*!
  *  \mainpage Programme pour appliquer l'algo du simplexe
- *  \author SERRES Valentin <serresvale@cy-tech.fr>
+ *  \author SERRES Valentin <serresvale@cy-tech.fr> BIGOT Léo-Paul <bigotleopa@cy-tech.fr>
  *  \section INFO
  *     Voici le code fais par Valentin Serres & Léo-Paul Bigot qui permet de faire l'algorithme du simplexe
 */
@@ -18,9 +18,26 @@
 /*Inclusion de la librairie stdlib.h qui permet à l'utilisateur de gérer la mémoire de façon dynamique ou encore de pouvoir intéragir avec le système d'exploitation*/
 #include <stdlib.h>
 
+/*!
+ *  \fn void liberation(float** tab, int taille)
+ *  \author SERRES Valentin <serresvale@cy-tech.fr> BIGOT Léo-Paul <bigotleopa@cy-tech.fr>
+ *  \version 0.1 Premier jet
+ *  \date Wed 29 2023 - 15:22:37
+ *  \brief procédure permettant de libérer la mémoire
+ *  \param tab pointeur de pointeur de réel représentant un tableau de réel 2 dimensions
+ *  \param taille entier représentant le nombre de ligne du tableau 2 dimensions
+*/
+
+void liberation(float** tab, int taille){
+    int i;
+    for(i=0;i<taille;i++){
+        free(tab[i]);
+    }
+    free(tab);
+}
 
 /*!
- *  \fn void affichhge(float** tab, int ligne, int colonne)
+ *  \fn void affichage(float** tab, int ligne, int colonne)
  *  \author SERRES Valentin <serresvale@cy-tech.fr> BIGOT Léo-Paul <bigotleopa@cy-tech.fr>
  *  \version 0.1 Premier jet
  *  \date Wed 29 2023 - 09:54:37
@@ -52,13 +69,8 @@ void affichage(float** tab, int ligne, int colonne){
  *  \param tab pointeur de pointeur de réel qui sert de tableau 2 dimensions
 */
 
-void initTab(int nbVar, int nbContrainte, float **tab){
-    float** newTab;
+void initTab(int nbVar, int nbContrainte, float **tab, float** newTab){
     int i,j;
-    newTab = malloc((nbContrainte+1)*sizeof *newTab);
-    for(i=0;i<(nbContrainte+1);i++)
-        newTab[i] = malloc((nbVar+nbContrainte+1)*sizeof newTab);
-
     for(i=0; i<(nbContrainte+1); i++){
         for(j=1; j<=nbVar; j++){
             newTab[i][j] = tab[i][j-1];
@@ -75,11 +87,31 @@ void initTab(int nbVar, int nbContrainte, float **tab){
         newTab[i][0] =tab[i][2]; 
     }
     affichage(newTab,(nbContrainte+1), (nbVar+nbContrainte+1));
-    for(i=0;i<(nbContrainte+1);i++){
-        free(newTab[i]);
-    }
-    free(newTab);
 }
+
+/*!
+ *  \fn float rechercheEntrant(float** newTab, int nbVar, int nbContrainte)
+ *  \author SERRES Valentin <serresvale@cy-tech.fr> BIGOT Léo-Paul <bigotleopa@cy-tech.fr>
+ *  \version 0.1 Premier jet
+ *  \date Wed 29 2023 - 15:07:38
+ *  \brief fonction permettant de chercher la variable entrante d'un tableau
+ *  \param newTab pointeur de pointeur de réel représentant un tableau de réel de 2 dimensions
+ *  \param nbVar entier représentant le nombre de variable du problème
+ *  \param nbContrainte entier représentant le nombre de contrainte du problème
+ *  \return max le réel qui représente la variable entrante
+*/
+
+float rechercheEntrant(float** newTab, int nbVar, int nbContrainte){
+    int i;
+    float max=0;
+    for(i=1;i<(nbVar + nbContrainte + 1); i++){
+        if(newTab[0][i]>max){
+            max = newTab[0][i];
+        }
+    }
+    return(max);
+}
+
 
 /*!
  *  \fn void simplexe()
@@ -90,8 +122,8 @@ void initTab(int nbVar, int nbContrainte, float **tab){
 */
 
 void simplexe(){
-    int i;
-    float** tab;
+    int i, nbVar, nbContrainte;
+    float** tab, **newTab;
     tab = malloc(4*sizeof *tab);
     for (i=0;i<4;i++)
         tab[i] = malloc(3*sizeof tab);
@@ -105,15 +137,22 @@ void simplexe(){
     tab[2][2] = 3;
     tab[3][0] = 1;
     tab[3][1] = -0.5;
-    tab[3][2] = 1;
+    tab[3][2] = 1;nbContrainte = 3;
+    nbContrainte = 3;
+    nbVar = 2;
+    newTab = malloc((nbContrainte+1)*sizeof *newTab);
+    for(i=0;i<=nbContrainte;i++){
+        newTab[i] = malloc((nbContrainte+nbVar+1)*sizeof newTab);
+    }
+
     affichage(tab, 4, 3);
     printf("\n");
-    initTab(2, 3, tab);
-    for(i=0;i<4;i++){
-        free(tab[i]);
-    }
-    free(tab);
+    initTab(2, 3, tab, newTab);
+    printf("%f \n", rechercheEntrant(newTab, nbVar, nbContrainte ));
+    liberation(newTab,nbContrainte+1);
+    liberation(tab, 4);
 }
+
 
 
 /*!
